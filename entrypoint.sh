9629cf5da1926a347d9123f83b093312c5821e9d
Steven Armstrong <steven.armstrong@id.ethz.ch>
2021-05-20 09:29:47 +0200

    add support for configuring master.cf
    Signed-off-by: Steven Armstrong <steven.armstrong@id.ethz.ch>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,51 @@
 #!/bin/sh
 
+#set -x
+
+# Apply configuration to /etc/postfix/master.cfg.
+if [ -f /config/master ]; then
+   while read -r line; do
+      [ -z "$line" ] && continue
+      [ "${line:0:1}" = "#" ] && continue
+      [ "${line:0:4}" = "#EOF" ] && break
+      # nuke leading and trailing whitespace
+      config="$(echo $line)"
+      case "$config" in
+         "-*")
+            # Support any of the master.cf related options like -M, -F, -P
+            echo "postconf $config"
+            postconf $config
+         ;;
+         *)
+            # Default to -M
+            echo "postconf -M $config"
+            postconf -M "$config"
+         ;;
+      esac
+   done < /config/master
+fi
+
+# Apply configuration to /etc/postfix/main.cfg.
+if [ -f /config/main ]; then
+   while read -r line; do
+      [ -z "$line" ] && continue
+      [ "${line:0:1}" = "#" ] && continue
+      [ "${line:0:4}" = "#EOF" ] && break
+      # nuke leading and trailing whitespace
+      config="$(echo $line)"
+      echo "postconf $config"
+      postconf "$config"
+   done < /config/main
+fi
+
+# Environment variables override configuration from /config/main.
 # POSTFIX_{name}={value} -> postconf {name}={value}
 env | grep ^POSTFIX_ | sed 's/^POSTFIX_//' \
 | while read -r config; do
    echo "postconf $config"
    postconf "$config"
 done
+
 
 # Log to stdout.
 postconf "maillog_file=/dev/stdout"
